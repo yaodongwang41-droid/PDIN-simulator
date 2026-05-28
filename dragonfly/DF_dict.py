b195bd3bbdcbe0350243b199d0ce6ee349c8913d
yaodongwang41-droid <yaodongwang41@gmail.com>
2026-05-28 14:38:05 +0900

    Update DF_dict.py

--- dragonfly/DF_dict.py
+++ dragonfly/DF_dict.py
@@ -1,28 +1,25 @@
 import random
-import numpy as np
 
 
 def dct(m, l):
     out = {}
     for i in range(l*m+1):  # number of groups
         for j in range(m):    # number of routers in one group
-            router ='0' * (len(str(l*m+1)) - len(str(i))) + str(i) + str(j)
-            out[router] = 0
+            router = [i, j]
+            out[tuple(router)] = 0
     return out
 
 
-def config(lam, k, m, l):
+def config(num, k, m, l):
     out = []
     a = list(dct(m, l).keys())    # total router IDs
-    node = np.zeros(3)
     for i in a:
-        node[0] = int(i[0:len(str(l*m+1))])
-        r = [int(x) for x in i[len(str(l*m+1)):len(i)]]
+        node = list(i)
         for j in range(k):
-            node[1:2], node[len(node)-1] = r, j
-            out.append(list([int(x) for x in node]))
+            node.append(j)
+            out.append(node)
     random.shuffle(out)
-    out = out[0:int(lam * (l*m+1)* m)*k]
+    out = out[0:num]
     return out
 
 
@@ -32,5 +29,7 @@ if __name__ == "__main__":
     L = 2
     dict = dct(M, L)
     print(len(dict), dict)
-    res = config(0.2, K, M, L)
+    lam = 0.2
+    nums = int(lam * (M*L+1) * M * K)
+    res = config(nums, K, M, L)
     print(len(res), res)
